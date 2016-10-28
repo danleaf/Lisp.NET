39fd1181e3e5a45a6a9849249b3ab15540de06b3
danleaf <danleaf@foxmail.com>
2016-10-28 17:27:57 +0800

    十大

--- Lexer/Lexer.fs
+++ Lexer/Lexer.fs
@@ -1,13 +1,37 @@
-﻿module Lexer
+﻿//module Lexer
 
-open System.Text
-open System.Linq
+open System
+
+type Token = 
+    | Empty
+    | Identifier
+    | Number
+    | Error
 
 type Status =
     | INIT
     | END
     | IDENTIFIER
     | NUMBER
+    | ERROR
+
+let cdr = List.tail
+let car = List.head
+
+let rec cdrn n l = 
+    if n = 0 then 
+        l
+    else 
+        match l with
+        | [] -> []
+        | _::tail -> cdrn (n-1) tail
+        
+
+let str2list (str:string) = 
+    str.ToCharArray() |> List.ofArray 
+
+let list2str (cl:char list) = 
+    new string(cl |> List.toArray)
 
 let (|Letter|_|) = function
     | x when x <= 'z' && x >= 'a' -> Some()
@@ -18,29 +42,69 @@ let (|Digit|_|) = function
     | x when x <= '9' && x >= '0' -> Some()
     | _ -> None
 
-let inittrans = function
-    | Letter -> IDENTIFIER
-    | Digit -> NUMBER
-    | _ -> INIT
-    
-let idtrans = function
-    | Letter -> IDENTIFIER
-    | Digit -> IDENTIFIER
-    | _ -> END
+let (|Point|_|) = function
+    | x when x = '.' -> Some()
+    | _ -> None
 
-let rec StatusTransition r len input currStatus = 
-    if currStatus = END then
-        (r, len)
-    else  
-        match input with 
-        | [] -> (r, len)
-        | h::tail -> 
-            let s =
-                match currStatus with 
-                | INIT -> inittrans h
-                | IDENTIFIER -> idtrans h
-                | _ -> END
-            let r' = if s = IDENTIFIER then [h] else []
-            StatusTransition (r @ r') (len+1) tail s
+let GetToken = function
+    | INIT -> Empty
+    | IDENTIFIER -> Identifier
+    | NUMBER -> Number
+    | ERROR -> Error
+    | _ -> Error
 
-let r = StatusTransition [] 0 ("  a0/".ToCharArray().ToList()) INIT
+let rec ReadToken' r len status = function
+    | h::t -> 
+        let next_status,is_valid_char = 
+            match status with
+            | INIT -> 
+                match h with
+                | Letter -> IDENTIFIER, true
+                | Digit -> NUMBER, true
+                | _ -> INIT, false
+
+            | IDENTIFIER -> 
+                match h with
+                | Letter -> IDENTIFIER, true
+                | Digit -> IDENTIFIER, true
+                | _ -> END, false
+
+            | NUMBER -> 
+                match h with
+                | Letter -> ERROR, true
+                | Digit -> NUMBER, true
+                | _ -> END, false
+
+            | ERROR -> 
+                match h with
+                | Letter -> ERROR, true
+                | Digit -> ERROR, true
+                | _ -> END, false
+
+            | _ -> END, false
+
+        let r' = if is_valid_char then [h] else []
+        if next_status = END then
+            GetToken status, (r @ r'), len
+        elif t = [] then
+            GetToken next_status, (r @ r'), (len + 1)
+        else
+            ReadToken' (r @ r') (len + 1) next_status t
+
+    | [] -> Empty, r, len        
+
+let ReadToken input = 
+    let t, r, len = ReadToken' [] 0 INIT input
+    t, (list2str r), (cdrn len input), len
+
+let rec Parse input =
+    let token, word, last,len = ReadToken input
+    printfn "%s\t%A\t%d" word token len
+    if last = [] then
+        ()
+    else
+        Parse last
+
+
+let input = str2list ";;   ;"
+Parse input
