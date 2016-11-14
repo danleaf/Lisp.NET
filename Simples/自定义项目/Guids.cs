bf0e65a9e9be0509fdc620df3282f10d22b3caea
danleaf <danleaf@foxmail.com>
2016-11-14 11:51:52 +0800

    123

---
+++ Simples/自定义项目/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Company.自定义项目
+{
+    static class GuidList
+    {
+        public const string guid自定义项目PkgString = "48adcff8-e3e7-4214-a48f-0c3aebced03c";
+        public const string guid自定义项目CmdSetString = "0b7499a9-8ca7-4b5f-890c-154df4894ed1";
+
+        public static readonly Guid guid自定义项目CmdSet = new Guid(guid自定义项目CmdSetString);
+    };
+}
